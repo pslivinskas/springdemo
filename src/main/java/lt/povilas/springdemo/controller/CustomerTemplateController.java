883fcbfec1ee64f00913bd893563faef058c9752
pslivinskas <povilaslsivinskas@gmail.com>
2023-01-17 16:57:47 +0200

    Added template firstpage.html

--- src/main/java/lt/povilas/springdemo/controller/CustomerTemplateController.java
+++ src/main/java/lt/povilas/springdemo/controller/CustomerTemplateController.java
@@ -1,9 +1,12 @@
 package lt.povilas.springdemo.controller;
 
+import lt.povilas.springdemo.repository.model.Customer;
 import lt.povilas.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,19 @@ public class CustomerTemplateController {
     @GetMapping(path = "/test")
     public String getTestPage() {
         return "/test/test_page";
+    }
+
+    // http://localhost:8080/customertemplate/firstpage/customer/112
+
+    @GetMapping(path = "/firstpage/customer/{id}")
+    public String getCustomerWithFirstPage(Model model, @PathVariable int id) {
+
+        Customer customer = customerService.getMyCustomerById(id);
+        model.addAttribute("company_name", customer.getCustomerName());
+        model.addAttribute("contact_last_name", customer.getContactLastName());
+        model.addAttribute("contact_first_name", customer.getContactFirstName());
+        model.addAttribute("country", customer.getCountry());
+
+        return "/test/firstpage";
     }
 }
